12395d99cbc3f69f6d890de63541c6f541ae16ca
Fausto G. Cintra <professor@faustocintra.com.br>
2022-03-26 11:02:28 -0300

    (26/03) Conexão ao banco de dados parametrizada com dotenv

---
+++ back-end/config/database.js
@@ -0,0 +1,22 @@
+// Conexão: Galaxy A10S4044
+// Senha: vvwm4919
+
+const mongoose = require('mongoose')
+
+module.exports = function() {
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERV}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+        // Parâmetros da conexão 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        //useFindAndModify: false
+    })
+
+    mongoose.connection.on('connected', () => 
+        console.log('** Mongoose! conectado ao servidor remoto')
+    )
+
+    mongoose.connection.on('error', erro => 
+        console.error('*** ERRO: Mongoose! não conectado ao servidor remoto. Causa: ' + erro)
+    )
+}
+
